0c76b2e7297033776d5f2497f025463fcb45cfe2
Bernd Strehl <bernd.strehl@gmail.com>
2021-09-28 09:27:39 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  function genItems(k) {
+    const added = [];
+    for (let i = 0; i < 1000 * k; i++) {
+      const obj = {
+        color: Math.random() > 0.5 ? "red" : "green",
+      };
+      added.push(obj);
+    }
+    console.log("hey");
+    setItems([...items, ...added]);
+  }
+
+  function filterGreen() {
+    const filtered = items.filter((x) => x.color === "green");
+    setItems(filtered);
+  }
+
+  function iterateRemaining() {
+    const propMap = new Map();
+    items.forEach((x) => {
+      propMap.set(x.color, true);
+    });
+    alert("done");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Filter Test</header>
+      <div>
+        <div>
+          <a onClick={() => genItems(10)}>Add 10k</a>
+          <a onClick={() => genItems(100)}>Add 100k</a>
+
+          <p>Current: {items.length}</p>
+        </div>
+
+        <div>
+          <a onClick={() => filterGreen()}>Filter Green</a>
+        </div>
+
+        <div>
+          <a onClick={() => iterateRemaining()}>Iterate Remaining</a>
+        </div>
+      </div>
     </div>
   );
 }
